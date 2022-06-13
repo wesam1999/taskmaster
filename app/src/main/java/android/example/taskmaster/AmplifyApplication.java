f5df9c5f0cbe92ad79d48aa21fb8c4566df358c5
wesam1999 <wesam.qandah1999@gmail.com>
2022-06-13 12:24:14 +0300

    solved lab 41

--- app/src/main/java/android/example/taskmaster/AmplifyApplication.java
+++ app/src/main/java/android/example/taskmaster/AmplifyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.amplifyframework.AmplifyException;
+import com.amplifyframework.analytics.pinpoint.AWSPinpointAnalyticsPlugin;
 import com.amplifyframework.api.aws.AWSApiPlugin;
 import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
 import com.amplifyframework.core.Amplify;
@@ -34,10 +35,12 @@ public class AmplifyApplication extends Application {
 
     private void configureAmplify() {
         try {
+
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSDataStorePlugin());
             Amplify.addPlugin(new AWSS3StoragePlugin());
+
             Amplify.configure(getApplicationContext());
 
 
--- app/src/main/java/android/example/taskmaster/TaskDetail.java
+++ app/src/main/java/android/example/taskmaster/TaskDetail.java
@@ -6,31 +6,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.predictions.models.LanguageType;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.time.Instant;
 
 public class TaskDetail extends AppCompatActivity {
 
+    private final MediaPlayer mp = new MediaPlayer();
+    private TextView textView2;
+    private TextView textView;
+    private TextView textView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
 
-        TextView textView = findViewById(R.id.textView5);
-        TextView textView2 = findViewById(R.id.textView8);
-        TextView textView3 = findViewById(R.id.textView6);
+        textView = findViewById(R.id.textView5);
+        textView2 = findViewById(R.id.textView8);
+        textView3 = findViewById(R.id.textView6);
+        Button speech=findViewById(R.id.button8);
+        Button Translating=findViewById(R.id.button9);
+
         ActionBar actionBar = getSupportActionBar();
 
 
         actionBar.setDisplayHomeAsUpEnabled(true);
-
+speech.setOnClickListener(view ->Text_to_speech() );
+        Translating.setOnClickListener(view ->Text_to_Translating() );
 
 
         Intent passedIntent = getIntent();
@@ -41,6 +61,20 @@ public class TaskDetail extends AppCompatActivity {
         textView.setText(tasks);
         textView2.setText(taskState);
         textView3.setText(taskBody);
+    }
+
+    private void Text_to_Translating() {
+
+        Amplify.Predictions.translateText(
+                textView3.getText().toString() ,
+                LanguageType.ENGLISH,
+                LanguageType.ITALIAN,
+                result -> Log.i("MyAmplifyApp", result.getTranslatedText()),
+                error -> Log.e("MyAmplifyApp", "Translation failed", error)
+        );
+
+
+
     }
 
 
@@ -82,5 +116,45 @@ public class TaskDetail extends AppCompatActivity {
         Intent settingsIntent = new Intent(this, MainActivity.class);
         startActivity(settingsIntent);
     }
+
+    private void playAudio(InputStream data) {
+        File mp3File = new File(getCacheDir(), "audio.mp3");
+
+        try (OutputStream out = new FileOutputStream(mp3File)) {
+            byte[] buffer = new byte[8 * 1_024];
+            int bytesRead;
+            while ((bytesRead = data.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+            mp.reset();
+            mp.setOnPreparedListener(MediaPlayer::start);
+            mp.setDataSource(new FileInputStream(mp3File).getFD());
+            mp.prepareAsync();
+        } catch (IOException error) {
+            Log.e("MyAmplifyApp", "Error writing audio file", error);
+        }
+
+    }
+
+public void Text_to_speech(){
+    Amplify.Predictions.convertTextToSpeech(
+            textView.getText().toString(),
+            result -> playAudio(result.getAudioData()),
+            error -> Log.e("MyAmplifyApp", "Conversion failed", error)
+    );
+
+    Amplify.Predictions.convertTextToSpeech(
+            textView2.getText().toString(),
+            result -> playAudio(result.getAudioData()),
+            error -> Log.e("MyAmplifyApp", "Conversion failed", error)
+    );
+    Amplify.Predictions.convertTextToSpeech(
+            textView3.getText().toString(),
+            result -> playAudio(result.getAudioData()),
+            error -> Log.e("MyAmplifyApp", "Conversion failed", error)
+    );
+
+}
+
 
 }
